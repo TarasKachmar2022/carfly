22f4b0184d96370337618c7931a93baf21f19951
Taras <kachmar.taras@gmail.com>
2024-07-30 22:57:11 +0300

    add sellers responsive and sellers graphic responsive

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,11 +2,11 @@ import Swiper from 'swiper';
 import 'swiper/swiper.scss';
 
 const swiper = new Swiper('.swiper', {
-  spaceBetween: 10,
   breakpoints: {
     0: {
       direction: 'vertical',
       slidesPerView: 2,
+      spaceBetween: 10,
       // allowTouchMove: false,
       // loop: false,
     },
