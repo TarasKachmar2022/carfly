b30b7c31c724c8a27c29d298c3934b7d42b7c5d0
Taras <kachmar.taras@gmail.com>
2024-08-03 13:05:15 +0300

    add mobile-menu.scss, nav.scss, add hero desktop

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,24 +1,33 @@
-import Swiper from 'swiper';
+import Swiper, { Navigation, Autoplay } from 'swiper';
 import 'swiper/swiper.scss';
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper-sellers', {
+  modules: [Navigation, Autoplay],
+  slidesPerView: 'auto',
+  autoplay: {
+    delay: 3000,
+    // reverseDirection: true,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   breakpoints: {
     0: {
       direction: 'vertical',
-      slidesPerView: 2,
       spaceBetween: 10,
+      slidesPerView: 4,
       // allowTouchMove: false,
       // loop: false,
     },
     768: {
       direction: 'horizontal',
+      speed: 2000,
       slidesPerView: 2,
       allowTouchMove: true,
       loop: true,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+      spaceBetween: 20,
     },
   },
 });
