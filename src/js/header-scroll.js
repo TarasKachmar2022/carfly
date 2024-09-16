e51bfe68bc0ef74cacb7edc0ea106d02a5cdfcf8
Taras <kachmar.taras@gmail.com>
2024-09-17 01:01:59 +0300

    update header-scroll

--- src/js/header-scroll.js
+++ src/js/header-scroll.js
@@ -3,12 +3,10 @@ window.addEventListener('scroll', changeHeaderBackground);
 function changeHeaderBackground() {
   const header = document.querySelector('[data-header]');
   const headerOffsetTrigger = header.offsetHeight - 100;
-  const pageOffset = window.pageYOffset;
+  const pageOffset = window.scrollY;
   const viewportWidth = window.innerWidth;
 
-  if (viewportWidth >= 768) {
-    return;
-  } else {
+  if (viewportWidth < 768) {
     if (pageOffset > headerOffsetTrigger) {
       header.classList.add('header--no-transparency');
     } else {
